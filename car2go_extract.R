5407a9fbdd3e7af62c4dc3ab755b9828d8a531f3
Taras Motulski <taras.motulski@gmail.com>
2017-01-16 23:48:31 +0300

    basic analize

--- car2go_extract.R
+++ car2go_extract.R
@@ -2,7 +2,7 @@ Sys.setlocale("LC_CTYPE", "German")
 
 require(jsonlite)
 
-setwd("c:/Projects/Carsharing/datasets/car2go-muc")
+setwd("d:/Projects/Carsharing/datasets/car2go-muc")
 
 files <- list.files(pattern = ".json")
 
@@ -23,7 +23,7 @@ all_vehicles_df <- data.frame(
 write.table(all_vehicles_df, "all_vehicles_df.csv", col.names=TRUE, row.names = FALSE, sep=",")
 
 for (fileCount in seq_along(files)) {
-
+#for (filecount in 18217:31035) {
   filename <- files[fileCount]
   
   #print progress
@@ -35,12 +35,10 @@ for (fileCount in seq_along(files)) {
   
   if (length(available_vehicles_df) > 0) {
     vehicles_df <- data.frame(
-      #datetime = strptime(substr(filename, 1, 18), "%Y-%m-%dT%H_%M_%S", tz="Europe/Berlin"),
       filename = substr(filename, 14, 32),
       name = available_vehicles_df$name,
       fuelLevel = available_vehicles_df$fuel, 
       address = available_vehicles_df$address,
-      #coordinates = c(available_vehicles_df$coordinates)[1],
       engineType = available_vehicles_df$engineType,
       exterior = available_vehicles_df$exterior,
       interior = available_vehicles_df$interior,
@@ -60,5 +58,55 @@ for (fileCount in seq_along(files)) {
   }
 }
 
+##calc rent interval
 
+all_vehicles_df <- read.csv("all_vehicles_df.csv",header=TRUE,row.names=NULL)
+all_vehicles_df <- cbind(all_vehicles_df, datetime = NA)
+all_vehicles_df$datetime <- strptime(all_vehicles_df$filename, "%Y-%m-%dT%H_%M_%S")
 
+all_vehicles_df <- unique(all_vehicles_df)
+all_vehicles_df <- na.omit(all_vehicles_df)
+
+vehicles_group_df <- unique(data.frame(car_name = all_vehicles_df$name))
+
+all_car_interval <- data.frame()
+all_rent_interval <- data.frame()
+
+carGroupCount <- nrow(vehicles_group_df)
+
+for (ii in 1:carGroupCount) {
+  
+  print(paste("Car #", ii, " of ", carGroupCount))
+  
+  car_data <- data.frame()
+  car_data <- all_vehicles_df[all_vehicles_df$name == vehicles_group_df$car_name[ii],]
+  car_data <- car_data[order(car_data$datetime),]
+  
+  car_interval <- data.frame(
+    name = car_data$name,
+    address = car_data$address,
+    engineType = car_data$engineType,
+    exterior = car_data$exterior,
+    interior = car_data$interior,
+    vin = car_data$vin,
+    smartphoneRequired = car_data$smartphoneRequired,
+    lat <- car_data$lat,
+    lat <- car_data$lng,
+    start_datetime = car_data$datetime,
+    end_datetime = c(car_data$datetime[-1], NA),
+    start_fuel = car_data$fuelLevel,
+    end_fuel = c(car_data$fuelLevel[-1], NA)
+  )
+  
+  car_interval$rent_time_min <- difftime(car_interval$end_datetime, car_interval$start_datetime,units="mins")  
+  car_interval$rent_fuel_loss <- round(car_interval$start_fuel - car_interval$end_fuel, digits = 2)
+  
+  all_car_interval <- rbind(all_car_interval, car_interval)  
+  #write.csv2(car_interval, file = "all_car_interval.csv", na = "NA", row.names = FALSE, append = TRUE)
+  
+  rent_interval <- car_interval[car_interval$trip_time > 5, ]
+  all_rent_interval <- rbind(all_rent_interval, rent_interval) 
+}
+
+write.table(all_car_interval, "all_car_interval.csv", col.names=TRUE, row.names = FALSE, sep=",")
+write.table(all_rent_interval, "all_rent_interval.csv", col.names=TRUE, row.names = FALSE, sep=",")
